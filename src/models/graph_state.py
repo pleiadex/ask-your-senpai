a2f2774725893334aa5876c673d2dbd1c1254bf5
Sungyoun Seo <pleiadex.dev@gmail.com>
2024-04-30 17:25:58 -0400

    add models

---
+++ src/models/graph_state.py
@@ -0,0 +1,15 @@
+from typing_extensions import TypedDict
+from typing import List
+
+class GraphState(TypedDict):
+    """
+    Represents the state of our graph.
+
+    Attributes:
+        question: question
+        generation: LLM generation
+        documents: list of documents
+    """
+    question : str
+    generation : str
+    documents : List[str]
